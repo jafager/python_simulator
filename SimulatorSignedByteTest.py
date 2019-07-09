15b2cca3fbee8a79435b587022bb1377408eb15b
Jason A. Fager <jafager@jafager.com>
2019-07-08 20:49:21 -0400

    Minor formatting changes Continued work on SimulatorXXX types and tests

--- SimulatorSignedByteTest.py
+++ SimulatorSignedByteTest.py
@@ -1,32 +1,32 @@
 import unittest
-from SimulatorUnsignedByte import SimulatorUnsignedByte
+from SimulatorSignedByte import SimulatorSignedByte
 from SimulatorExceptions import ValueOutOfRangeException
 
 
-class SimulatorUnsignedByteTest(unittest.TestCase):
+class SimulatorSignedByteTest(unittest.TestCase):
 
     def test_constructor(self):
 
         # Test default value
 
-        simulator_type = SimulatorUnsignedByte()
+        simulator_type = SimulatorSignedByte()
         self.assertEqual(simulator_type.get(), simulator_type.default)
 
         # Test initial value
 
-        simulator_type = SimulatorUnsignedByte(simulator_type.minimum)
+        simulator_type = SimulatorSignedByte(simulator_type.minimum)
         self.assertEqual(simulator_type.get(), simulator_type.minimum)
 
-        simulator_type = SimulatorUnsignedByte(simulator_type.maximum)
+        simulator_type = SimulatorSignedByte(simulator_type.maximum)
         self.assertEqual(simulator_type.get(), simulator_type.maximum)
 
         # Test invalid initial value
 
         with self.assertRaises(ValueOutOfRangeException):
-            simulator_type = SimulatorUnsignedByte(simulator_type.minimum - 1)
+            simulator_type = SimulatorSignedByte(simulator_type.minimum - 1)
 
         with self.assertRaises(ValueOutOfRangeException):
-            simulator_type = SimulatorUnsignedByte(simulator_type.maximum + 1)
+            simulator_type = SimulatorSignedByte(simulator_type.maximum + 1)
 
 
 if __name__ == '__main__':
